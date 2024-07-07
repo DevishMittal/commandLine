d932aad22e9bc6682aba0ba236109a37377b1bc2
Devish Mittal <devishmittal1@gmail.com>
2024-07-07 23:22:23 +0530

    better grammer

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::io;
 
 fn main() {
-    println!("enter your weight on earth: ");
+    println!("Enter your weight on earth (Kg): ");
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
     let weight: f32 = input.trim().parse().unwrap();
-    print!("Entered weight {}", calc(weight));
+    print!("Your weight on Mars: {}", calc(weight));
 }
 fn calc(weight: f32) -> f32 {
     (weight / 9.81) * 3.711
