9b61dec95c8b84bed3799a8eef1285f6f18c8d73
Devish Mittal <devishmittal1@gmail.com>
2024-07-07 23:20:06 +0530

    simple cmdLine weight on mars calc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("enter your weight on earth: ");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let weight: f32 = input.trim().parse().unwrap();
+    print!("Entered weight {}", calc(weight));
+}
+fn calc(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711
 }
